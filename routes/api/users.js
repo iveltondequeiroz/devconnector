5412678a2f6e90e39c35b65ec933f1404882a61b
ivelton queiroz <junglemantx@gmail.com>
2019-07-04 11:40:03 -0400

    validator middleware

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-// @route 	GET api/users
-// @desc 	User route test
+// @route 	POST api/users
+// @desc 	Register User
 // @Acess 	Public
-router.get('/', (req, res) => res.send('Users router'));
+router.post('/', [
+	check('name', 'Name is required').not().isEmpty(),
+	check('email', 'Please include valid email').isEmail(),
+	check('password', 'Please inform password with 6 or more characters')
+		.isLength({min: 6})
+],(req, res) => {
+	const errors = validationResult(req);
+	console.log('errors', errors);
+	if(!errors.isEmpty()) {
+		return res.status(400).json( {errors: errors.array() });
+	}
+	res.send('User registered!')
+});
 
 module.exports = router;
