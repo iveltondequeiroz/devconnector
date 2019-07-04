53b1d5c65e37943b2fabfd786b0ba28e47ecf7bc
ivelton queiroz <junglemantx@gmail.com>
2019-07-04 17:35:08 -0400

    json web token

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../../models/User');
 
@@ -48,7 +50,22 @@ async (req, res) => {
 
 		await user.save();
 
-		res.send('User registered')
+		const payload = {
+			user: {
+				id: user.id
+			}
+		}
+
+		jwt.sign(payload, 
+			config.get('jwtSecret'), 
+			{ expiresIn : 360000},
+			(err, token) => {
+				if(err) throw err;
+				res.json({ token });
+			}
+
+		); 
+
 	} catch(err) {
 		console.error("Err:", err.message);
 		res.status(500).send('Server Error');
